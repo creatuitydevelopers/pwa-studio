85db703375cddc84d25c21fd98037a58b4d30fcf
mdonarski <mdonarski@creatuity.pl>
2019-02-28 12:58:12 +0100

    [RKPWA-76] Product interface should serve additional attributes for custom product page sections

--- packages/venia-concept/src/components/RkStore/index.js
+++ packages/venia-concept/src/components/RkStore/index.js
@@ -2,6 +2,7 @@ export { StoresList } from './List';
 export { Info } from './Info';
 export { Title } from './Title';
 export { ChooseStoreButton, StoreDetailsButton } from './Buttons';
+export { MoreInformation } from './MoreInformation';
 export { MyStoreBadge } from './MyStoreBadge';
 export { Tile } from './Tile';
 export { DetailsPage } from './DetailsPage';
