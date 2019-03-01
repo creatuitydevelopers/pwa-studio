6ac2001e695b8c0e114d2d2afad945b9412ce884
Krzysiek Bogus <bogus.krzysiek@gmail.com>
2019-03-01 07:16:14 +0100

    fix

--- packages/venia-concept/src/components/Weeklyad/Weeklyad.js
+++ packages/venia-concept/src/components/Weeklyad/Weeklyad.js
@@ -3,25 +3,25 @@ import React from 'react';
 const flippContainerId = 'flipp-container';
 
 const loadIframe = () => {
-    var aScript = document.createElement('script');
-        aScript.type = 'text/javascript';
-        aScript.src = "https://circular.ruralking.com/hosted_services/iframe.js";
-        document.head.appendChild(aScript);
-        window.addEventListener('load', function() {
-            if (wishabi) {
-                wishabi.hostedservices.iframe.decorate(
-                    flippContainerId,
-                    'ruralking',
-                    wishabi.hostedservices.iframe.Sizing.PAGE,
-                    {
-                        minHeight: 600,
-                        initialHeight: 600,
-                        extraPadding: 0,
-                        queryParameters: ''
-                    }
-                );
-            }
-        })
+    let aScript = document.createElement('script');
+    aScript.type = 'text/javascript';
+    aScript.src = "https://circular.ruralking.com/hosted_services/iframe.js";
+    document.head.appendChild(aScript);
+    window.addEventListener('load', function() {
+        if (wishabi) {
+            wishabi.hostedservices.iframe.decorate(
+                flippContainerId,
+                'ruralking',
+                wishabi.hostedservices.iframe.Sizing.PAGE,
+                {
+                    minHeight: 600,
+                    initialHeight: 600,
+                    extraPadding: 0,
+                    queryParameters: ''
+                }
+            );
+        }
+    })
 }
 
 class Weeklyad extends React.Component {
