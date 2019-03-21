2d44dc797e42c11e3ed953be8f8f631faf221df7
mdonarski <mdonarski@creatuity.pl>
2019-03-21 13:05:52 +0100

    [RKPWA-100] Error with delivery methods appearing in console should be resolved

--- packages/venia-concept/src/components/DeliveryMethods/components/DeliveryMethods/DeliveryMethods.js
+++ packages/venia-concept/src/components/DeliveryMethods/components/DeliveryMethods/DeliveryMethods.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
-import {func, object, shape, string, oneOf, number} from 'prop-types';
-
+import {func, object, shape, string, oneOf} from 'prop-types';
 import classify from 'src/classify';
-import defaultClasses from './deliveryMethods.css';
+
+import {withApollo} from 'react-apollo';
+import gql from 'graphql-tag';
+
 import DeliveryMethodsList from 'src/components/DeliveryMethods/components/DeliveryMethodsList';
 import {isStsMethod, isCurrentStoreEnabledForSts, isDeliveryMethodValid} from 'src/models/DeliveryMethods';
 
-import gql from 'graphql-tag';
-import {Query} from 'react-apollo';
+import defaultClasses from './deliveryMethods.css';
+
 
 const searchQuery = gql`
     query($sku: String) {
@@ -43,95 +45,106 @@ class DeliveryMethods extends Component {
         viewMode: 'product-page'
     };
 
+    state = {
+        errors: false,
+        loading: true,
+        methods: []
+    };
+
+    async componentDidMount() {
+        const {client, defaultMethod, onChange, selectedStore, currentStore, productSku} = this.props;
+
+        client.query({
+            query: searchQuery,
+            variables: {
+                sku: productSku
+            }
+        }).then((response) => {
+            const store = !!selectedStore ? selectedStore : currentStore;
+            const methods = response.data.deliveryMethods;
+
+            if (methods.length > 0 && !defaultMethod) {
+                methods.every(method => {
+                    if (isStsMethod(method.method) && !isCurrentStoreEnabledForSts(method.stores, store)) return true;
+                    if (!isDeliveryMethodValid(method.method, store)) return true;
+
+                    onChange(method.method, store);
+                    return false;
+                });
+            }
+
+            this.setState({
+                methods,
+                loading: response.loading,
+                error: false
+            })
+        }).catch((error) =>{
+            this.setState({
+                error: !!error
+            });
+        });
+    }
+
     async componentDidUpdate(prevProps) {
         const {currentStore, onChange} = this.props;
 
-        if (!!prevProps.currentStore && !!currentStore && prevProps.currentStore.store_number != currentStore.store_number) {
+        if (!!prevProps.currentStore && !!currentStore && prevProps.currentStore.store_number !== currentStore.store_number) {
             onChange(null, null);
         }
     };
 
-    render() {
+    get content(){
         const {
-            defaultMethod,
-            productSku,
             currentStore,
-            selectedStore,
-            validationMessage,
+            defaultMethod,
             onChange,
-            classes,
+            productSku,
+            selectedStore,
             viewMode
         } = this.props;
-        const store = !!selectedStore ? selectedStore : currentStore;
+
+        const {loading, methods, error} = this.state;
+
+        if(!!error) return <div>Something went wrong. Please refresh page.</div>;
+        if(!!loading) return <DeliveryMethodsList methods={Array.from({length: 1}).fill(null)}/>;
+
+        //Temporary, until checkout process on backend size would resolve problem with one product with multi delivery methods
+        // const methods = response.data.deliveryMethods;
+        const filteredMethods = methods.filter((method) => {
+            const cartItems = this.props.cart.details.items;
+
+            if (typeof cartItems === 'undefined' || cartItems.length === 0) return true;
+
+            const productItems =  cartItems.filter((item) => item.sku === productSku);
+
+            if(!productItems.length) return true;
+
+            return productItems.some((item) => method.method === item.extension_attributes.delivery_method);
+        });
+
+        return <DeliveryMethodsList
+            viewMode={viewMode}
+            methods={filteredMethods}
+            defaultMethod={defaultMethod}
+            selectedStore={!!selectedStore ? selectedStore : currentStore}
+            onChange={onChange}
+        />
+    }
+
+
+    render() {
+        const { validationMessage, classes } = this.props;
 
         return (
             <section className={classes.root}>
                 <h2 className={classes.header}>
                     <span>Delivery Methods</span>
                 </h2>
-                <Query query={searchQuery} variables={{sku: productSku}}>
-                    {({loading, error, data}) => {
-                        if (error)
-                            return (
-                                <div>
-                                    Something went wrong. Please refresh page.
-                                </div>
-                            );
-                        if (loading)
-                            return (
-                                <DeliveryMethodsList
-                                    methods={Array.from({length: 1}).fill(
-                                        null
-                                    )}
-                                />
-                            );
-                        //Temporary, until checkout process on backend size would resolve problem with one product with multi delivery methods
-                        // const methods = data.deliveryMethods;
-                        const methods = data.deliveryMethods.filter((method) => {
-                            const cartItems = this.props.cart.details.items;
-                            if (typeof cartItems === 'undefined' || cartItems.length == 0) {
-                                return true;
-                            }
-
-                            const productItems =  cartItems.filter((item) => item.sku == productSku)
-                            if(!productItems.length){
-                                return true;
-                            }
-
-                            return productItems.some((item) => {
-                                return method.method == item.extension_attributes.delivery_method
-                            });
-                        });
-
-                        if (methods.length > 0 && !defaultMethod) {
-                            methods.every(method => {
-                                if (isStsMethod(method.method) && !isCurrentStoreEnabledForSts(method.stores, store)) return true;
-                                if (!isDeliveryMethodValid(method.method, store)) return true;
-
-                                onChange(method.method, store);
-                                return false;
-                            });
-                        }
-
-                        return (
-                            <DeliveryMethodsList
-                                viewMode={viewMode}
-                                methods={methods}
-                                defaultMethod={defaultMethod}
-                                selectedStore={store}
-                                onChange={onChange}
-                            />
-                        );
-                    }}
-                </Query>
-                {!!validationMessage && (
-                    <p className={classes.validationMessage}>
-                        {validationMessage}
-                    </p>
-                )}
+                {this.content}
+                {!!validationMessage && <p className={classes.validationMessage}>{validationMessage}</p>}
             </section>
         );
     }
 }
 
-export default classify(defaultClasses)(DeliveryMethods);
+export default withApollo(classify(defaultClasses)(DeliveryMethods));
